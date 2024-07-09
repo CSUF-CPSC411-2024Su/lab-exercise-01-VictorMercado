ef74ed707b54aea8a0f8f8fd4bb09e01a81ad6a2
Victor Mercado <victormercado@outlook.com>
2024-07-09 10:44:26 -0700

    doing a quick test

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -11,11 +11,13 @@ protocol Volunteer: ObservableObject {
     var age: Int { get set }
     var maxHours: Int { get }
 }
+// test
 
-// TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
+    // TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
+    
+    // TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
+    
+    // TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
+    
+    // TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
 
-// TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
-
-// TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
-
-// TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
